7d13f78e638750413d13c749066e502e10adda54
fhalbiero <fhalbiero@gmail.com>
2019-09-13 17:03:47 -0300

    update our menu-items to navigate withRouter using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
-import HomePage from './pages/homepage/homepage'
+import HomePage from './pages/homepage/homepage';
+
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 
 function App() {
-  return <div>
-      <HomePage />
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+     
     </div>
+  );
 }
 
 export default App;
--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -49,13 +49,9 @@ class Directory extends React.Component {
     render() {
         return (
             <div className="directory-menu">
-                {this.state.sections.map(({ title, id, imageUrl, size }) => {
+                {this.state.sections.map(({ id, ...otherSectionProps }) => {
                     return (
-                        <MenuItem 
-                            key={id} 
-                            title={title} 
-                            imageUrl={imageUrl}
-                            size={size}
+                        <MenuItem  key={id} {...otherSectionProps}
                         />)
                 })} 
             </div>
