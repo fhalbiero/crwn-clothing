6e9d6df9c1bfe9262e435344d35f40e7986abf4a
fhalbiero <fhalbiero@gmail.com>
2019-10-07 16:46:09 -0300

    Updates

---
+++ precache-manifest.dc594ddfae56cbc497111af7dc7a7d3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f2a51911a3e692f7c890eddd7c1b945",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "7deeea33c3993b4d99c1",
+    "url": "/crwn-clothing/static/css/main.7bfc7eb1.chunk.css"
+  },
+  {
+    "revision": "082dcfd33fcd6bcac8bd",
+    "url": "/crwn-clothing/static/js/2.5bd0c660.chunk.js"
+  },
+  {
+    "revision": "7deeea33c3993b4d99c1",
+    "url": "/crwn-clothing/static/js/main.d89ac283.chunk.js"
+  },
+  {
+    "revision": "9a78e82df8f8529e1571",
+    "url": "/crwn-clothing/static/js/runtime~main.7afac7a2.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
